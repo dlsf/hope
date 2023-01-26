8bdac266e6c6ac5b6dac8eb33d396e090f81f87e
Nick Hensel <nickhensel25@icloud.com>
2023-01-26 15:11:37 +0100

    chore: Setup repository, gradle and project

---
+++ hope/src/main/java/io/github/madethoughts/hope/Application.java
@@ -0,0 +1,13 @@
+package io.github.madethoughts.hope;
+
+/**
+ The programms main entry
+ */
+public final class Application {
+    private Application() {
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
